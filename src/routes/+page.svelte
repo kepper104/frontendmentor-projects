cdba84a069800260301b3b32c2723b3ffb3545db
kepper104 <67190416+kepper104@users.noreply.github.com>
2024-04-17 23:35:20 +0300

    Initial setup; Basic landing page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+    import { onMount } from "svelte"
+
+    const travelDurationMs = 500
+
+    onMount(() => {
+        let mouseFollower = document.getElementById("following")! // dangerous
+
+        // document.body.onpointerleave = (e) => {
+        //     setTimeout(() => {
+        //         mouseFollower.hidden = true
+        //     }, travelDurationMs)
+        // }
+
+        document.body.onpointermove = (e) => {
+            let { clientX, clientY } = e
+
+            clientX = Math.min(Math.max(clientX, 100), window.innerWidth - 150)
+            clientY = Math.min(Math.max(clientY, 100), window.innerHeight - 150)
+
+            mouseFollower.hidden = false
+
+            mouseFollower.animate(
+                {
+                    left: `${clientX}px`,
+                    top: `${clientY}px`
+                },
+                { duration: travelDurationMs, fill: "forwards" }
+            )
+        }
+    })
+</script>
+
+<div id="following" class=""></div>
+
+<div class="rubik-normal custom-bg flex h-full w-full flex-col bg-slate-900 p-20 text-slate-200">
+    <h1 class="z-1 text-6xl">Hello there!</h1>
+
+    <h2 class="z-1 mt-5 text-3xl text-slate-300">
+        Here you can preview all my projects from practicing on <a
+            href="https://www.frontendmentor.io"
+            class="font-bold underline underline-offset-4 hover:translate-x-0">Frontend Mentor</a
+        >!
+    </h2>
+    <div class="z-1 mt-10 flex w-fit flex-col gap-10 pl-10 text-4xl">
+        <!--        // put links here-->
+    </div>
+</div>
+
+<!--<a href="https://www.flaticon.com/free-icons/curriculum-vitae" title="curriculum vitae icons">Curriculum vitae icons created by Abdul-Aziz - Flaticon</a>-->
+
+<style>
+    a {
+        @apply transition-all hover:translate-x-4 hover:text-slate-400;
+    }
+
+    #following {
+        position: absolute;
+        width: 300px;
+        height: 300px;
+        left: 50%;
+        top: 50%;
+        translate: -50% -50%;
+        border-radius: 50%;
+        background: rgb(30, 137, 237);
+        background: radial-gradient(circle, rgba(30, 137, 237, 1) 0%, rgba(70, 252, 129, 0.6) 100%);
+        filter: blur(40px);
+        opacity: 10%;
+        pointer-events: none;
+    }
+</style>
